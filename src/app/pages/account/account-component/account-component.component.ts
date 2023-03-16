1b0047751fec9066b211981a89a24b6175d79482
ferdousr3 <ferdousr1992@gmail.com>
2023-03-16 16:49:05 +0600

    add new folder

---
+++ src/app/pages/account/account-component/account-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-component',
+  templateUrl: './account-component.component.html',
+  styleUrls: ['./account-component.component.scss']
+})
+export class AccountComponentComponent {
+
+}
